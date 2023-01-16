c951c1497e9844c9b17971d36a4678f3d91439a5
Florenc13 <khisyametdinov.ruslan@gmail.com>
2023-01-16 14:34:36 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arrE = expr.split('');
+    let s = 2;
+    let dotArr1 = [];
+    let points = [];
+    let s2 = 5;
+    let dotArr2 = [];
+    let letter = [];
+    let wArr = [];
+    let result = [];
+    
+    
+    for (let i= 0; i < arrE.length/s; i++){
+        dotArr1[i] = arrE.slice((i*s), (i*s) + s )
+        points[i] = dotArr1[i].join('');
+    }
+    
+    points.forEach(item => {
+        if (item === '00') {
+            dotArr2.push('');        
+        } else if (item === '10') {
+            dotArr2.push('.');
+        } else if ( item === '11') {
+            dotArr2.push('-');
+        } else {
+        dotArr2.push(' ');
+        }
+    })
+    
+    for (let i = 0; i <dotArr2.length/s2; i++){
+    letter[i] = dotArr2.slice((i*s2), (i*s2) + s2 );
+    }
+
+    letter.forEach(item => {wArr.push((item.join('')));})
+    wArr.forEach(item => {result.push(MORSE_TABLE[item])})
+
+return result.join('');
+    
 }
 
 module.exports = {
